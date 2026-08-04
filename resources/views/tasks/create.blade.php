0bd95b1eabdffbabe78bddf07d9fd92af772e558
ghostrabbir0703 <rummypa123@gmail.com>
2026-08-04 03:19:38 -0400

    vistas de tasks create,update,show,index y show

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-</div>
+@extends('layout.app')
+
+@section('title','Crear tarea')
+
+@section('content')
+
+<h1 class="mb-4">Nueva tarea</h1>
+
+<form action="{{ route('tasks.store') }}" method="POST">
+
+    @csrf
+
+    @include('tasks._form')
+
+    <button class="btn btn-success">
+        Guardar
+    </button>
+
+    <a href="{{ route('tasks.index') }}" class="btn btn-secondary">
+        Cancelar
+    </a>
+
+</form>
+
+@endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+@extends('layout.app')
+
+@section('title','Editar tarea')
+
+@section('content')
+
+<h1 class="mb-4">Editar tarea</h1>
+
+<form
+    action="{{ route('tasks.update',$task) }}"
+    method="POST"
+>
+
+    @csrf
+    @method('PUT')
+
+    @include('tasks._form')
+
+    <button class="btn btn-warning">
+        Actualizar
+    </button>
+
+    <a href="{{ route('tasks.index') }}" class="btn btn-secondary">
+        Cancelar
+    </a>
+
+</form>
+
+@endsection
